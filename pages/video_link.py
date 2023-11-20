294852248092e844c47f79094823b284352825f7
Luis David Tribiño <ltribino@gmail.com>
2023-11-20 10:24:59 +0100

    docs: Document function implementations

--- pages/video_link.py
+++ pages/video_link.py
@@ -6,8 +6,8 @@ from controllers.comment_processor import extract_comments_and_languages
 st.title("Insert a youtube video link")
 video_link = st.text_input("video_link", label_visibility="hidden")
 if st.button("Send"):
-    # la func
     df = extract_comments_and_languages(video_link)
-    st.title(video_link)
-    columns_to_show = ["text", "Detected_Language"]
+    # TODO buscar y mostrar el titulo del video
+    # st.title(video_link)
+    columns_to_show = ["text", "Detected_Language", "Translated_Text"]
     st.dataframe(df[columns_to_show])
